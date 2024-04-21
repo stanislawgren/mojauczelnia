07a60894227f9fd2105ae14bb01889031eae5156
Turbobot <turbobot@vercel.com>
2024-04-21 12:28:17 +0200

    feat(create-turbo): apply official-starter transform

--- apps/api/src/app.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const app = express();
-
-app.get("/", (req, res) => res.send("It works!"));
-
-if (import.meta.env.PROD) app.listen(3000);
-
-export const viteNodeApp = app;
