c5587b293e3d1061ec11e80ae61ae99a9cd753f5
Mateusz Flis <flismati.s@gmail.com>
2024-04-23 13:14:09 +0200

    Database integration

--- apps/api/index.js
+++ apps/api/index.js
@@ -9,3 +9,11 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+let sql = require('./db.js')
+
+sql.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('Database test: ', rows[0].solution)
+})
