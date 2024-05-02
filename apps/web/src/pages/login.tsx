6920946b855014e355d462cb0699ebeb32b33558
stanislawgren <stanislawg1200@gmail.com>
2024-05-02 20:45:18 +0200

    JWT working solution

--- apps/web/src/pages/login.tsx
+++ apps/web/src/pages/login.tsx
@@ -1,18 +1,50 @@
 // import logo from "./../assets/auth_logo.svg";
 
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+
 export const LoginPage = () => {
-    return (
-      <div className="app-container">
-        <div className="auth-background"></div>
-        <div className="auth__container">
-          <div className="auth__container-component">
-            {/* <img src={logo} alt="logo" style={{ width: "50%" }} /> */}
-            <p style={{ fontSize: "30px" }}>Logowanie</p>
-            <input type="text" className="main-input" placeholder="E-Mail" />
-            <input type="password" className="main-input" placeholder="Hasło" />
-            <button className="blue-button">Zaloguj się</button>
-          </div>
+  const auth = useAuth();
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleLogin = async () => {
+    const result = await auth.signIn(email, password);
+
+    console.log(result);
+  };
+
+  return (
+    <div className="app-container">
+      <div className="auth-background"></div>
+      <div className="auth__container">
+        <div className="auth__container-component">
+          {/* <img src={logo} alt="logo" style={{ width: "50%" }} /> */}
+          <p style={{ fontSize: "30px" }}>Logowanie</p>
+          <input
+            type="text"
+            className="main-input"
+            placeholder="E-Mail"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            className="main-input"
+            placeholder="Hasło"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button className="blue-button" onClick={handleLogin}>
+            Zaloguj się
+          </button>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
