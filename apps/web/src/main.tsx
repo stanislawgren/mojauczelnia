b3f47baf17cd70dbba2cd0697197d5db68bbfb94
Aleksandra Gabka <aleksandraxgabka@gmail.com>
2024-05-28 03:04:32 +0200

    user profile page created

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MainPage } from "./pages";
 import { LoginPage } from "./pages/login";
 import { RegisterPage } from "./pages/register";
+import { UserProfilePage } from "./pages/userProfile";
 import { AuthProvider } from "./hooks/useAuth";
 import { CookiesProvider } from "react-cookie";
 import { RequireAuth } from "./components/RequireAuth";
@@ -17,7 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          
+          <Route path="/profile" element={<UserProfilePage />} />
           {/* Example protected route */}
           <Route path="/example" element={
             <RequireAuth>
