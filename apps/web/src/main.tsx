2e7072f3f9d75d5bb93e0a55aed06bbd38e6e0a3
Mateusz Flis <flismati.s@gmail.com>
2024-05-28 03:07:36 +0200

    Frontend pagination hook

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./../public/styles/index.scss";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { MainPage } from "./pages";
-import { LoginPage } from "./pages/login";
-import { RegisterPage } from "./pages/register";
-import { AuthProvider } from "./hooks/useAuth";
-import { CookiesProvider } from "react-cookie";
-import { RequireAuth } from "./components/RequireAuth";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {MainPage} from "./pages";
+import {LoginPage} from "./pages/login";
+import {RegisterPage} from "./pages/register";
+import {AuthProvider} from "./hooks/useAuth";
+import {RequireAuth} from "./components/RequireAuth";
+import {ExamplePaginationPage} from "./pages/_example_pagination.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -17,7 +17,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          
+
+          {/* Example pagination */}
+          <Route path="/pagination" element={<ExamplePaginationPage />} />
+
           {/* Example protected route */}
           <Route path="/example" element={
             <RequireAuth>
